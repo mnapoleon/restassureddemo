e6ab9cf52d5fb0ed4eb996a397bcf537b83e593f
Michael Napoleon <michael.napoleon@gmail.com>
2024-03-24 12:09:04 -0400

    initial commit

---
+++ src/main/java/dev/michaelnapoleon/restassureddemo/RestassureddemoApplication.java
@@ -0,0 +1,13 @@
+package dev.michaelnapoleon.restassureddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestassureddemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestassureddemoApplication.class, args);
+  }
+
+}
